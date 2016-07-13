1e6ca8bc42d0e5c5b8ca1e7c01b5d690eaaccf06
Devon Henegar <devon.henegar@gmail.com>
2016-07-13 15:12:45 -0400

    welcome pages and messages added

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,27 @@
+class MessagesController < ApplicationController
+  def index
+    @messages = Message.all
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+
+    if @message.save
+      flash[:notice] = "Message Sent"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "There was an error with the message, please try again"
+      render :new
+    end
+  end
+
+
+  private
+    def message_params
+      params.require(:message).permit(:body, :response_type, :response_email, :response_phone, :response_name)
+    end
+end
