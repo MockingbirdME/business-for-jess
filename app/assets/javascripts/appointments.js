d145a31ad72ed026bba59269b9766417a21d9beb
Richard Newman <richard@newmanworks.com>
2016-07-23 14:11:30 -0600

    improve js files

--- app/assets/javascripts/appointments.js
+++ app/assets/javascripts/appointments.js
@@ -1,17 +1,16 @@
-window.onload = function(){
-
-
+$('document').ready( function(){
+console.log("++++++++++++++++++++appointments++++++++++++++++++++++++++++");
 
   var appointment_select_box = document.getElementById('appointment_type_select');
-  var dog_walking_options_div = document.getElementById('dog_walking_options');
-  var small_animal_hotel_options_div = document.getElementById('small_animal_hotel_options');
-  var in_home_animal_care_options_div = document.getElementById('in_home_animal_care_options');
-console.log("++++++++++++++++++++appointments++++++++++++++++++++++++++++");
+  if (appointment_select_box) {
 console.log(appointment_select_box);
-  appointment_select_box.addEventListener('change', function(){
-console.log("+++++++++++++++++++++appointments+++++++++++++++++++++++++++");
+    appointment_select_box.addEventListener('change', function(){
+console.log("+++++++++++++++++++++appointments#change+++++++++++++++++++++++++++");
+    var dog_walking_options_div = document.getElementById('dog_walking_options');
+    var small_animal_hotel_options_div = document.getElementById('small_animal_hotel_options');
+    var in_home_animal_care_options_div = document.getElementById('in_home_animal_care_options');
     var select_value = 9;
-    var select_value = appointment_select_box.value;
+    select_value = appointment_select_box.value;
 console.log(select_value);
     if (select_value == 'dog_walking') {
       dog_walking_options_div.style.display='block';
@@ -29,3 +28,4 @@ console.log(select_value);
     return true;
   });
 }
+});
