c53b05f5cdfcaf15fee9887fbefa09998590efaa
Devon Henegar <devon.henegar@gmail.com>
2016-07-26 13:34:23 -0400

    wip

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -7,6 +7,32 @@ class Appointment < ActiveRecord::Base
 
   enum appointment_type: [:dog_walking, :small_animal_hotel, :in_home_animal_care]
 
+
+  def self.time_slots(day)
+    apps = Appointment.where(start_time: day.to_datetime..((day.to_datetime)+(1.day)))
+    apps = apps.order(:start_time)
+    first = day.midnight+(12.hours)
+    last = day.midnight+(20.hours)
+    openings = []
+    unless apps.empty?
+      if apps.first.start_time > (day.midnight+(12.hours)+(45.minutes))
+        openings << [first, (apps.first.start_time-(15.minutes))]
+      end
+      if apps.count > 1
+        ((apps.count)-1).times do |n|
+          if ((apps[n].start_time)+((apps[n].duration+60).minutes)) <= apps[n+1].start_time
+            openings << [((apps[n].start_time)+((apps[n].duration+15).minutes)), (apps[n+1].start_time-(15.minutes))]
+          end
+        end
+      end
+      if (apps.last.start_time + (apps.last.duration + 45).minutes) <= last
+        openings << [apps.last.start_time+((apps.last.duration+15).minutes),last]
+      end
+    end
+    openings
+  end
+
+
   def validate_appointment
     returned = []
     returned << "Appointment requires a start time" if self.start_time.blank?
@@ -21,7 +47,6 @@ class Appointment < ActiveRecord::Base
     end_of_potential_apt= potential_time+((potential_duration+15).minutes)
     appointments_to_check = Appointment.where(start_time: earliest_potential_conflict..end_of_potential_apt).select("start_time, duration")
     if appointments_to_check.empty?
-
       return false
     else
       appointments_to_check.any? do |app|
