48f7e9cf24fb5d3ce11f520668ec1f3800a092ee
Devon Henegar <devon.henegar@gmail.com>
2016-07-29 16:28:27 -0400

    owner may now schedule days off

--- app/assets/javascripts/appointments.js
+++ app/assets/javascripts/appointments.js
@@ -7,19 +7,28 @@ console.log("+++++++++++++++++++++appointments#change+++++++++++++++++++++++++++
     var dog_walking_form_div = document.getElementById('dog_walking_form');
     var small_animal_hotel_form_div = document.getElementById('animal_hotel_form');
     var in_home_animal_care_form_div = document.getElementById('in_home_form');
-
+    var time_off_div = document.getElementById('time_off_form')
     if (this.value == 'dog_walking') {
       dog_walking_form_div.style.display='block';
       small_animal_hotel_form_div.style.display='none';
       in_home_animal_care_form_div.style.display='none';
+      time_off_div.style.display='none';
     } else if (this.value == 'small_animal_hotel') {
       dog_walking_form_div.style.display='none';
       small_animal_hotel_form_div.style.display='block';
       in_home_animal_care_form_div.style.display='none';
-    } else {
+      time_off_div.style.display='none';
+    } else if (this.value == 'in_home_animal_care') {
       dog_walking_form_div.style.display='none';
       small_animal_hotel_form_div.style.display='none';
-      in_home_animal_care_form_div.style.display='block';    }
+      in_home_animal_care_form_div.style.display='block';
+      time_off_div.style.display='none';
+    } else if (this.value == 'time_off') {
+      dog_walking_form_div.style.display='none';
+      small_animal_hotel_form_div.style.display='none';
+      in_home_animal_care_form_div.style.display='none';
+      time_off_div.style.display='block';
+    }
     return true;
   });
 });
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @appointment = Appointment.new
   end
 end
