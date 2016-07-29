34c1ff2e3ac31b487a4ecd96b67bf4b0f48d5f2b
Devon Henegar <devon.henegar@gmail.com>
2016-07-29 11:26:29 -0400

    calendars now working, fixed user name in contact us view but

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -8,7 +8,7 @@ class Appointment < ActiveRecord::Base
 
   def self.time_slots(day)
     apps = Appointment.where(start_time: day.to_datetime..((day.to_datetime)+(1.day)))
-    apps = apps.order(:start_time)
+    apps = apps.where(appointment_type: 0).order(:start_time)
     first = day.midnight+(16.hours)
     last = day.midnight+(19.hours)
     openings = []
@@ -32,35 +32,92 @@ class Appointment < ActiveRecord::Base
     openings
   end
 
+  def self.hotel_vacancy?(day)
+    guests = Appointment.where(appointment_type: 1)
+    guests = guests.where(start_time: ((day.to_datetime.midnight)-(13.days))..day.to_datetime.midnight)
+    guests_for_night = 0
+    unless guests.empty?
+      guests.each do |app|
+        if app.start_time.to_date ==  day.to_date || ((app.start_time.to_date+app.duration.days)>day)
+          guests_for_night += 1
+        end
+      end
+    end
+    guests_for_night >= 3 ? false : true
+  end
+
+  def self.in_home_time?(day)
+    apps = Appointment.where(appointment_type: 1)
+    apps = apps.where(start_time: ((day.to_datetime.midnight)-(13.days))..day.to_datetime.midnight)
+    homes_for_the_day = 0
+    unless apps.empty?
+      apps.each do |app|
+        if app.start_time.to_date ==  day.to_date || ((app.start_time.to_date+app.duration.days)>day)
+          homes_for_the_day += 1
+        end
+      end
+    end
+    if day.to_date.saturday? || day.to_date.sunday?
+      homes_for_the_day >= 3 ? false : true
+    else
+      homes_for_the_day >+ 1 ? false : true
+    end
+  end
 
   def validate_appointment
     returned = []
     returned << "Appointment requires a start time" if self.start_time.blank?
-    returned << "Appointment date can not be in the past" if (self.start_time < Time.current)
-    returned << "Please book by phone for appointments within the next two hours, we can be reached at (207)-555-5555" if ((self.start_time >= Time.current)&&(self.start_time < Time.current + 2.hours))
+    returned << "Appointment date can not be in the past" if (self.start_time < Date.current)
+    returned << "To ensure our promptness please book your appointments for today by phone, we can be reached at (555-555-5555)" if (self.start_time.to_date == Date.current )
     returned
   end
-
 
   def dog_walking_conflict?(potential_time,potential_duration)
     earliest_potential_conflict = potential_time-(135.minutes)
     end_of_potential_apt= potential_time+((potential_duration+15).minutes)
     appointments_to_check = Appointment.where(start_time: earliest_potential_conflict..end_of_potential_apt).select("start_time, duration")
+    appointments_to_check = appointments_to_check.where(appointment_type: 0)
     if appointments_to_check.empty?
       return false
     else
       appointments_to_check.any? do |app|
         within_appointment = (app.start_time < potential_time)
-Rails.logger.info("*****#{app.start_time}*********#{potential_time}***************** #{within_appointment}")
         within_appointment &&=  (app.start_time + ((app.duration+15).minutes) > potential_time)
-Rails.logger.info("******************************* #{within_appointment}")
         during_appointment = (app.start_time > potential_time)
-Rails.logger.info("******************************* #{during_appointment}")
         during_appointment &&= (app.start_time < potential_time+((potential_duration+15).minutes))
-Rails.logger.info("******************************* #{during_appointment}")
         within_appointment || during_appointment
       end
     end
   end
+
+  def small_animal_hotel_conflict?(potential_time, potential_duration)
+    conflicts = []
+    potential_duration.times do |n|
+      date = potential_time.to_date+(n.days)
+      conflicts << date if (Appointment.hotel_vacancy?(date) == false)
+    end
+    conflicts.empty? ? false : true
+  end
+
+  def in_home_animal_care_conflict?(potential_time, potential_duration)
+    conflicts = []
+    potential_duration.times do |n|
+      date = potential_time.to_date+(n.days)
+      conflicts << date if (Appointment.in_home_time?(date) == false)
+    end
+    conflicts.empty? ? false : true
+  end
+
+  def conflict?(potential_time, potential_duration)
+    case appointment_type
+    when 'dog_walking'
+      dog_walking_conflict?(potential_time, potential_duration)
+    when 'small_animal_hotel'
+      small_animal_hotel_conflict?(potential_time, potential_duration)
+    when 'in_home_animal_care'
+      in_home_animal_care_conflict?(potential_time, potential_duration)
+    end
+  end
+
 
 end
